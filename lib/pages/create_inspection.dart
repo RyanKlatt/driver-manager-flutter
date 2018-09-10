69e3b9595c6403a0cc2bed0afe65094ff826cf77
Ryan Klatt <rpklatt@msn.com>
2018-09-09 21:53:17 -0600

    finished up add inspection page

--- lib/pages/create_inspection.dart
+++ lib/pages/create_inspection.dart
@@ -17,11 +17,28 @@ class _InspectionCreateState extends State<InspectionCreatePage> {
   var inspectionTimeController = new TextEditingController();
   var dateController = new TextEditingController();
   var truckController = new TextEditingController();
+  var odometerController = new TextEditingController();
+  var otherIssuesController = new TextEditingController();
 
-  String typeValue;
+  bool isEngineChecked = false;
+  bool isTransmissionChecked = false;
+  bool isClutchChecked = false;
+  bool isSteeringMechChecked = false;
+  bool isHornChecked = false;
+  bool isWipersChecked = false;
+  bool isWindshieldChecked = false;
+  bool isMirrorsChecked = false;
+  bool isLightingChecked = false;
+  bool isParkingChecked = false;
+  bool isBrakesChecked = false;
+  bool isTiresChecked = false;
+  bool isWheelsChecked = false;
+  bool isEmergencyChecked = false;
+  bool isRegistrationChecked = false;
+  bool isAnnualChecked = false;
+  bool isOilChecked = false;
+
   String issueValue;
-
-  bool _isChecked = false;
 
   int _radioValue = 0;
 
@@ -83,24 +100,249 @@ class _InspectionCreateState extends State<InspectionCreatePage> {
   }
 
   _buildExpansionTile() {
-    return DropdownButtonHideUnderline(
-      child: new DropdownButton<String>(
-        hint: Text('Select Truck Issues'),
-        style: TextStyle(fontSize: 20.0, color: Theme.of(context).accentColor),
-        value: issueValue,
-        onChanged: (String newValue) {
-          setState(() {
-            issueValue = newValue;
-          });
-        },
-        items: <String>['Engine', 'Transmission', 'Clutch'].map((String value) {
-          return new DropdownMenuItem<String>(
-            value: value,
-            child: new Text(value),
-          );
-        }).toList(),
-      ),
-    );
+    return Center(
+        child: new Column(
+      children: <Widget>[
+        Divider(),
+        SizedBox(height: 10.0),
+        Text(
+          'Select Truck Issues',
+          style: TextStyle(fontSize: 18.0),
+        ),
+        SizedBox(height: 10.0),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isEngineChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isEngineChecked = newValue;
+                });
+              },
+            ),
+            Text('Engine'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isTransmissionChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isTransmissionChecked = newValue;
+                });
+              },
+            ),
+            Text('Transmission'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isClutchChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isClutchChecked = newValue;
+                });
+              },
+            ),
+            Text('Clutch'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isSteeringMechChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isSteeringMechChecked = newValue;
+                });
+              },
+            ),
+            Text('Steering Mechanism'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isHornChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isHornChecked = newValue;
+                });
+              },
+            ),
+            Text('Horn'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isWipersChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isWipersChecked = newValue;
+                });
+              },
+            ),
+            Text('Windshield Wipers'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isWindshieldChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isWindshieldChecked = newValue;
+                });
+              },
+            ),
+            Text('Windshield Cracked'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isMirrorsChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isMirrorsChecked = newValue;
+                });
+              },
+            ),
+            Text('Mirrors'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isLightingChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isLightingChecked = newValue;
+                });
+              },
+            ),
+            Text('Lights/Reflectors'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isParkingChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isParkingChecked = newValue;
+                });
+              },
+            ),
+            Text('Parking Brake'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isBrakesChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isBrakesChecked = newValue;
+                });
+              },
+            ),
+            Text('Brakes'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isTiresChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isTiresChecked = newValue;
+                });
+              },
+            ),
+            Text('Tires'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isWheelsChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isWheelsChecked = newValue;
+                });
+              },
+            ),
+            Text('Wheels/Rims'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isEmergencyChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isEmergencyChecked = newValue;
+                });
+              },
+            ),
+            Text('Emergency Equipment'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isRegistrationChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isRegistrationChecked = newValue;
+                });
+              },
+            ),
+            Text('Registration/Tags Expired'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isAnnualChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isAnnualChecked = newValue;
+                });
+              },
+            ),
+            Text('Annual Inspection Expired'),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Checkbox(
+              value: isOilChecked,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isOilChecked = newValue;
+                });
+              },
+            ),
+            Text('Oil Change Needed'),
+          ],
+        ),
+        new Divider(),
+        TextField(
+          style:
+              TextStyle(fontSize: 20.0, color: Theme.of(context).accentColor),
+          controller: otherIssuesController,
+          decoration: InputDecoration(
+            labelText: 'Other Issues',
+            labelStyle: TextStyle(fontSize: 20.0),
+          ),
+        ),
+      ],
+    ));
   }
 
   @override
@@ -151,30 +393,11 @@ class _InspectionCreateState extends State<InspectionCreatePage> {
               ),
             ),
             SizedBox(height: 10.0),
-            DropdownButtonHideUnderline(
-              child: new DropdownButton<String>(
-                hint: Text('Inspection Type'),
-                style: TextStyle(
-                    fontSize: 20.0, color: Theme.of(context).accentColor),
-                value: typeValue,
-                onChanged: (String newValue) {
-                  setState(() {
-                    typeValue = newValue;
-                  });
-                },
-                items: <String>['Pre-Trip', 'Post-Trip', 'Other']
-                    .map((String value) {
-                  return new DropdownMenuItem<String>(
-                    value: value,
-                    child: new Text(value),
-                  );
-                }).toList(),
-              ),
-            ),
             TextField(
               keyboardType: TextInputType.number,
               style: TextStyle(
                   fontSize: 20.0, color: Theme.of(context).accentColor),
+              controller: odometerController,
               decoration: InputDecoration(
                 labelText: 'Odometer',
                 labelStyle: TextStyle(fontSize: 20.0),
